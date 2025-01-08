da78bc440eb5761e808f97da455e7e2223f265f1
MostafaElzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-01-08 16:04:10 +0200

    Home screen and its widgets

---
+++ lib/features/home/data/models/place_model.dart
@@ -0,0 +1,12 @@
+class Place {
+  String name;
+  String location;
+  String imageUrl;
+  bool isFavorite;
+
+  Place(
+      {required this.name,
+      required this.location,
+      required this.imageUrl,
+      required this.isFavorite});
+}
